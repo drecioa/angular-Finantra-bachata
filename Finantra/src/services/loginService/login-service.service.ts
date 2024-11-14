a1fa402f6f767d7d23eb8c66d73c237ef7406b7e
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-14 15:15:35 +0100

    implementamos el servicio login
    este servicio se encargara de la comunicacion entre la web y el api (login)

--- Finantra/src/services/loginService/login-service.service.ts
+++ Finantra/src/services/loginService/login-service.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { LoginDto } from '../../models/loginDto';
-import { User } from '../../models/User';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class LoginService {
   private apiUrl="http://localhost:8080/api/v1/auth/login"; /**introduce la url */
   constructor(private http: HttpClient) { }
 
-  checkUser(email:String, password:String):Observable<User>{
-    return this.http.post<User>(this.apiUrl, new LoginDto(email, password));
+  checkUser(email:String, password:String):Observable<any>{
+    return this.http.post<any>(this.apiUrl, new LoginDto(email, password));
   }
 }
