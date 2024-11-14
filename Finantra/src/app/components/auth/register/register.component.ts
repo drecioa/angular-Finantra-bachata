0254c60b20e42934a2ce7aa744410497a607de7b
ricardo537 <dawricardo735@gmail.com>
2024-11-14 13:16:03 +0100

    Obtención de la respuesta del formulario
    Ahora se puede recoger la respuesta del formulario

--- Finantra/src/app/components/auth/register/register.component.ts
+++ Finantra/src/app/components/auth/register/register.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm:FormGroup;
+  
+  constructor(private formGroup: FormBuilder) {
+    this.registerForm = this.formGroup.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]]
+    });
+  }
 
+  submitRegister(): void {
+    if (this.registerForm.valid) {
+      console.log("Respuesta enviada: ", this.registerForm.value);
+    } else {
+      console.log("Respuesta no enviada");
+    }
+  }
 }
