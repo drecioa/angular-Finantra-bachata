432b0cbcedcf4f8cae3b0b09cbe0bfc32b4f6cb4
ricardo537 <dawricardo735@gmail.com>
2024-11-14 15:33:29 +0100

    Funcionamiento del servicio

--- Finantra/src/app/components/auth/register/register.component.ts
+++ Finantra/src/app/components/auth/register/register.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  providers: [RegisterService]
 })
 export class RegisterComponent {
   registerForm:FormGroup;
+  registerStatus: string | null = null;
   
-  constructor(private formGroup: FormBuilder) {
+  constructor(private formGroup: FormBuilder, private registerService:RegisterService) {
     this.registerForm = this.formGroup.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -22,7 +25,14 @@ export class RegisterComponent {
 
   submitRegister(): void {
     if (this.registerForm.valid) {
-      console.log("Respuesta enviada: ", this.registerForm.value);
+      this.registerService.register(this.registerForm.value).subscribe(
+        (response) => {
+          console.log(response);
+        }, 
+        (error) => {
+          console.log(error);
+        }
+      )
     } else {
       console.log("Respuesta no enviada");
     }
