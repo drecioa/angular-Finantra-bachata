df795ac400f7adc37c4cb29e68bf93c1b7553ff6
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-19 15:28:58 +0100

    impl redirect

--- Finantra/src/components/header/header.component.ts
+++ Finantra/src/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HomeComponent } from "../home/home.component";
+import { AuthService } from '@services/authService/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +11,10 @@ import { HomeComponent } from "../home/home.component";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  private auth=inject(AuthService);
+  private redirect=inject(Router);
+  logout():void{
+    this.auth.logout();
+    this.redirect.navigate(["/auth"]);
+  }
 }
