fe8bd41a040c7f4f8d480c3abc2819a2bd022085
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-19 14:57:28 +0100

    limpieza
    eliminamos cosas que no se usa

--- Finantra/src/app/app.component.ts
+++ Finantra/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { SessionComponent } from '@components/session/session.component';
-import { HeaderComponent } from "../components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [SessionComponent, RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
