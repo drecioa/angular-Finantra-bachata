7129992c95f6ae188abd30c0dd21bd61b550af94
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-19 12:34:10 +0100

    creacion de rutas

--- Finantra/src/app/app.routes.ts
+++ Finantra/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from '@components/login/login.component';
+import { RegisterComponent } from '@components/register/register.component';
+import { SessionComponent } from '@components/session/session.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    {path:"session", component:SessionComponent,
+        children: [
+            {path: 'login', component:LoginComponent},
+            {path: 'register', component:RegisterComponent}
+        ]
+    },
+    {path:"home", component:AppComponent},
+    {path:"", redirectTo:'/session', pathMatch:"full"}
+
+];
+
+export const rutas= provideRouter(routes);
--- Finantra/src/main.ts
+++ Finantra/src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
+import { rutas } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule, BrowserModule)  
+    importProvidersFrom(HttpClientModule),
+    rutas
   ]
 })
   .catch((err) => console.error(err));
