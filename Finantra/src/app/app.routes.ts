f8a9eb6c08186418ba451580b03feb97fb396993
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-20 09:16:32 +0100

    routing para landing page de session y title
    añadir nueva ruta para landing page de session y title para todos las paginas

--- Finantra/src/app/app.routes.ts
+++ Finantra/src/app/app.routes.ts
@@ -5,16 +5,18 @@ import { RegisterComponent } from '@components/register/register.component';
 import { SessionComponent } from '@components/session/session.component';
 import { homeGuard, sessionGuard } from 'src/guards/session.guard';
 import { HeaderComponent } from '@components/header/header.component';
+import { SessionMainPageComponent } from '@components/session/session-main-page/session-main-page.component';
 
 const routes: Routes = [
     {path:"auth", component:SessionComponent,
         children: [
-            {path: 'login', component:LoginComponent},
-            {path: 'register', component:RegisterComponent}
+            {path: "", component: SessionMainPageComponent, title:"Inicia sesión"},
+            {path: 'login', component:LoginComponent, title:"Login"},
+            {path: 'register', component:RegisterComponent, title: "Registro"}
         ],
         canActivate:[sessionGuard]
     },
-    {path:"home", component:HeaderComponent, canActivate:[homeGuard]},
+    {path:"home", component:HeaderComponent, canActivate:[homeGuard], title:'Finantra'},
     {path:"", redirectTo:'/auth', pathMatch:"full"},
     {path: "**", redirectTo:'/home'}
 
