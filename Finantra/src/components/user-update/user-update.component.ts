5a1a8b5d1cd794b8a6e35622c55cbc399834f55d
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-21 12:51:55 +0100

    formulario e impl funcion update

--- Finantra/src/components/user-update/user-update.component.ts
+++ Finantra/src/components/user-update/user-update.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '@models/User';
+import { UpdateService } from '@services/updateService/update-service.service';
+import { UtilsService } from '@services/utilsService/utils.service';
 
 @Component({
   selector: 'app-user-update',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-update.component.html',
   styleUrl: './user-update.component.css'
 })
 export class UserUpdateComponent {
+  protected user:User={
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:""
+  }
+  constructor(private update:UpdateService, private util:UtilsService){
+    const userFromStorage=sessionStorage.getItem("user");
+    if(userFromStorage){
+      this.user=JSON.parse(userFromStorage);
+    }
+  }
+
+  updateUserMethod(form:NgForm):void{
+    const userAux:User=form.value;
+
+    this.update.updateUser(userAux.email, userAux).subscribe(
+      (response)=>{
+        console.log(response);
+        this.util.auth.login(userAux);
+        this.user=userAux;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+  }
 
 }
