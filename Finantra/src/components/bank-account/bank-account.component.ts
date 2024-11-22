ebbc46ecf353a8414c9715435f05f847b1cbe10e
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-22 12:18:47 +0100

    impl function to bankAccount Component
    funciones como la de mostrar esta implementado aunque por ahora no funciona por el back. Hemos añadido un par de cosas como el targetAccount para dar pie el modificar y editar cuentas

--- Finantra/src/components/bank-account/bank-account.component.ts
+++ Finantra/src/components/bank-account/bank-account.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Bank } from '@models/Bank';
+import { BankService } from '@services/bankService/bank.service';
+import { UtilsService } from '@services/utilsService/utils.service';
 
 @Component({
   selector: 'app-bank-account',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bank-account.component.html',
   styleUrl: './bank-account.component.css'
 })
-export class BankAccountComponent {
+export class BankAccountComponent implements OnInit{
+  //No funciona por ahora*******************************************************
+  protected targetAccount!:Bank;
+  protected accounts:Bank[]=[
+    { accountId: "id",
+      providerId: "providerId",
+      bankName: "bankName",
+      iban: "iban",
+      currency: "currency?",
+      balance: 0,
+      notes: "Escrituras ancestrales"}
+  ];
 
+  constructor (private utils:UtilsService, /*private bankService:BankService*/){}
+
+  ngOnInit(): void {
+      /*
+      this.bankService.bank.subscribe(
+        (data)=>{
+          this.accounts=JSON.parse(data);
+        }, (error)=>{
+          console.error(error);
+        }
+      )*/
+  }
+  //WARNING: Aun no funciona------------------------------------------------------
+
+
+  setTargetAccount(cuenta: Bank):void{
+    this.targetAccount=cuenta;
+  }
 }
