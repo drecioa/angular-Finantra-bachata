88ac919101379bb9d706c1641133d4eb10e47066
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-22 12:36:40 +0100

    arreglar bug

--- Finantra/src/components/bank-account/bank-account.component.ts
+++ Finantra/src/components/bank-account/bank-account.component.ts
@@ -13,20 +13,36 @@ import { UtilsService } from '@services/utilsService/utils.service';
 })
 export class BankAccountComponent implements OnInit{
   //No funciona por ahora*******************************************************
-  protected targetAccount!:Bank;
-  protected accounts:Bank[]=[
-    { accountId: "id",
-      providerId: "providerId",
-      bankName: "bankName",
-      iban: "iban",
-      currency: "currency?",
-      balance: 0,
-      notes: "Escrituras ancestrales"}
-  ];
+  protected targetAccount:Bank={ 
+    accountId: "",
+    providerId: "",
+    bankName: "",
+    iban: "",
+    currency: "",
+    balance: 0,
+    notes: ""};
+  protected accounts:Bank[]=[];
 
   constructor (private utils:UtilsService, /*private bankService:BankService*/){}
 
   ngOnInit(): void {
+    //temporal
+    this.accounts=[
+      { accountId: "id",
+        providerId: "providerId",
+        bankName: "bankName",
+        iban: "iban",
+        currency: "currency?",
+        balance: 0,
+        notes: "Escrituras ancestrales"},
+      { accountId: "id2",
+        providerId: "providerId2",
+        bankName: "bankName2",
+        iban: "iban2",
+        currency: "currency?2",
+        balance: 0,
+        notes: "Escrituras ancestrales segunda parte"}
+    ]
       /*
       this.bankService.bank.subscribe(
         (data)=>{
