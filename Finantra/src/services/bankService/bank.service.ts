5fb0519674e66ba8f44352fc347f885b8b3a6854
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-22 12:17:20 +0100

    funcion getCuentaBancaria

--- Finantra/src/services/bankService/bank.service.ts
+++ Finantra/src/services/bankService/bank.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { BankAccountDTO } from '@models/bank-account-dto';
 import { User } from '@models/User';
 import { LoginDto } from '@models/loginDto';
+import { Bank } from '@models/Bank';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankService {
   private apiConnectUrl = 'http://localhost:8080/api/v1/bank/connect'; 
-  private apiSaveUrl = 'http://localhost:8080/api/v1/bank/save'; 
+  private apiSaveUrl = 'http://localhost:8080/api/v1/bank/save';
+  private apiGetUrl = 'http://localhost:8080/api/v1/bank/accounts';  
+
   protected user: User | null = null; 
+  
+  //No funciona por ahora*******************************************************
+  private behavior=new BehaviorSubject<any>(this.getAccounts(new LoginDto("", "")));
+  bank=this.behavior.asObservable();
+  //WARNING: Aun no funciona------------------------------------------------------
 
   constructor(private http: HttpClient) {
     // Obtener el usuario de sessionStorage
@@ -69,4 +77,22 @@ export class BankService {
     return this.http.post(this.apiSaveUrl, body, { headers });
   }
   
+  //No funciona por ahora*******************************************************
+  private getAccounts(login: LoginDto): Observable<any>{
+    const params= new HttpParams()
+      .set("email", login.email)
+      .set("password", login.password);
+    return this.http.get(this.apiGetUrl, {params});//???????????????
+  }
+
+  refreshAccounts(login: LoginDto):void{
+    this.getAccounts(login).subscribe(
+      (data)=>{
+        this.behavior.next(data.data);
+      }, (error)=>{
+        console.error(error);
+      }
+    )
+  }
+  //WARNING: Aun no funciona------------------------------------------------------
 }
