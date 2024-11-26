0c91f5153bf76c01798e119401a1c930d87d4bf1
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-11-26 14:21:34 +0100

    guardado y mostrado de noticias

--- Finantra/src/components/user-update/user-update.component.ts
+++ Finantra/src/components/user-update/user-update.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { LoginDto } from '@models/loginDto';
 import { Topic } from '@models/Topic';
 import { User } from '@models/User';
 import { NewsService } from '@services/newsService/news-service.service';
@@ -17,6 +18,8 @@ import { UtilsService } from '@services/utilsService/utils.service';
 })
 
 export class UserUpdateComponent implements OnInit{
+  protected listTopic:Topic[]=[];
+  private listOriginTopic:Topic[]=[];
   protected user:User={
     firstName:"",
     lastName:"",
@@ -42,6 +45,29 @@ export class UserUpdateComponent implements OnInit{
     )
   }
 
+  addTopicToList(value: string):void{
+    if(this.listTopic.filter(x=>x.name===value).length==0 
+      && this.topics.filter(x=>x.name===value).length!=0){
+
+      this.listTopic.push(new Topic(value));
+      const inputElement = document.getElementById("newTopic") as HTMLInputElement;
+      inputElement.value = "";
+
+    }
+  }
+
+  deleteTopicOfList(value:string){
+    this.listTopic= this.listTopic.filter(element=>element.name!=value)
+  }
+
+  saveTopics(){
+    this.newsService.saveTopics(new LoginDto(this.user.email, this.user.password), this.listTopic).subscribe(
+      (data)=>{
+        console.log(data);
+        this.util.redirect.navigate(["home/news"])
+      }, (error)=>{console.error(error);}
+    )
+  }
 
   ngOnInit(): void {
       this.util.auth.data.subscribe(
@@ -54,6 +80,16 @@ export class UserUpdateComponent implements OnInit{
         (data)=>{
           this.topics=data.data;
         }, (error)=>{console.error(error);
+        }
+      )
+
+      
+      this.newsService.getUserTopics(new LoginDto(this.user.email, this.user.password)).subscribe(
+        (data)=>{
+          this.listOriginTopic=data.data;
+          if(this.listOriginTopic.length!=0){
+            this.listTopic= this.listOriginTopic.slice();
+          }
         }
       )
   }
