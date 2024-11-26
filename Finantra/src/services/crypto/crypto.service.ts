faf0867edb1ba59388fc02108a5a3c9ee4ed5818
ricardo537 <dawricardo735@gmail.com>
2024-11-26 11:46:32 +0100

    Buscar crypto y añadirlas a la base de datos completado

--- Finantra/src/services/crypto/crypto.service.ts
+++ Finantra/src/services/crypto/crypto.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '@models/User';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { map, Observable, catchError, of } from 'rxjs';
 import { CryptoDto } from '@models/crypto-dto';
 
 @Injectable({
@@ -10,7 +10,9 @@ import { CryptoDto } from '@models/crypto-dto';
 
 export class CryptoService {
 
-  private apiSearchUrl: 'http://localhost:8080/api/v1/crypto/search?query='
+  private apiSearchUrl = 'http://localhost:8080/api/v1/crypto/search?query=';
+  private apiSaveUrl = 'http://localhost:8080/api/v1/crypto/add';
+
   protected user: User | null = null; 
 
   constructor(private http: HttpClient) {
@@ -22,6 +24,30 @@ export class CryptoService {
   }
 
   searchCryptos (query:string): Observable<CryptoDto[]> {
-    return this.http.get<{ data: CryptoDto[] }>(this.apiSearchUrl+query).pipe(map(response => response.data as CryptoDto[]));
+    return this.http.get<{ data: CryptoDto[] }>(`${this.apiSearchUrl}${query}`).pipe(
+      map((response) => response.data || []),
+      catchError((error) => {
+        console.error('Error al buscar criptomonedas:', error);
+        return of([]); 
+      })
+    );
+  }
+
+  saveCrypto (parameters:string): Observable<any> {
+    if (!this.user) {
+      console.error('No se ha encontrado el usuario en sessionStorage');
+      return new Observable(); 
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    const body = {
+      email: this.user.email, 
+      password: this.user.password
+    };
+
+    return this.http.post(`${this.apiSaveUrl}${parameters}`, body, { headers });
   }
 }
