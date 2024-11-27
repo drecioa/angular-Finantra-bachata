4a9e0723fb840f1ac3c90798dd489531c140a6b8
ricardo537 <dawricardo735@gmail.com>
2024-11-27 11:48:23 +0100

    Actualización constante de las estadísticas generales

--- Finantra/src/components/estadistica-general/estadistica-general.component.ts
+++ Finantra/src/components/estadistica-general/estadistica-general.component.ts
@@ -1,7 +1,9 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, AfterViewInit, OnInit  } from '@angular/core';
 import { Chart, ChartType } from 'chart.js/auto';
 import { BankService } from '@services/bankService/bank.service';
 import { BankAccountDTO } from '@models/bank-account-dto';
+import { CryptoService } from '@services/cryptoService/crypto.service';
+import { Crypto } from '@models/crypto';
 
 @Component({
   selector: 'app-estadistica-general',
@@ -10,29 +12,41 @@ import { BankAccountDTO } from '@models/bank-account-dto';
   templateUrl: './estadistica-general.component.html',
   styleUrl: './estadistica-general.component.css'
 })
-export class EstadisticaGeneralComponent implements AfterViewInit {
+export class EstadisticaGeneralComponent implements OnInit {
   public chart: Chart | undefined;
-  private accounts: BankAccountDTO[] = [];
+  private updateInterval: any; 
 
-  constructor(private bankService: BankService) {}
+  constructor(private bankService: BankService, private cryptoService: CryptoService) {}
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
+    this.updateChartData(); 
+    this.startAutoUpdate(); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.updateInterval) {
+      clearInterval(this.updateInterval);
+    }
+  }
+
+  updateChartData(): void {
     this.bankService.getAllAccounts().subscribe(
       (dataBank: BankAccountDTO[]) => {
-        this.accounts = dataBank;
-        console.log('Cuentas bancarias:', this.accounts);
-
-        const totalBalance = dataBank.reduce((sum, account) => sum + account.balance, 0);
-        this.createChart(totalBalance);
+        this.cryptoService.getAllCryptos().subscribe(
+          (dataCrypto: Crypto[]) => {
+            const totalBalanceBank = dataBank.reduce((sum, account) => sum + account.balance, 0);
+            const totalValueCrypto = dataCrypto.reduce((sum, crypto) => sum + crypto.totalValue, 0);
+            this.createChart(totalBalanceBank, totalValueCrypto);
+          }
+        );
       },
       (error) => {
         console.error('Error al obtener las cuentas bancarias:', error);
       }
     );
-    
   }
 
-  createChart (dataBank) {
+  createChart (dataBank, dataCrypto): void {
     const canvas = document.getElementById("chart-general") as HTMLCanvasElement;
     if (canvas) {
       const ctx = canvas.getContext("2d");
@@ -42,7 +56,7 @@ export class EstadisticaGeneralComponent implements AfterViewInit {
           datasets: [
             {
               label: 'General',
-              data: [dataBank, 50],
+              data: [dataBank, dataCrypto],
               backgroundColor: [
                 'rgb(255, 99, 132)',
                 'rgb(54, 162, 235)'
@@ -52,10 +66,15 @@ export class EstadisticaGeneralComponent implements AfterViewInit {
           ],
         };
 
-        this.chart = new Chart(ctx, {
-          type: 'pie' as ChartType,
-          data: data,
-        });
+        if (this.chart) {
+          this.chart.data = data; 
+          this.chart.update(); 
+        } else {
+          this.chart = new Chart(ctx, {
+            type: 'pie' as ChartType,
+            data: data,
+          });
+        }
       } else {
         console.error("No se pudo obtener el contexto 2D del canvas.");
       }
@@ -64,4 +83,9 @@ export class EstadisticaGeneralComponent implements AfterViewInit {
     }
   }
 
+  startAutoUpdate(): void {
+    this.updateInterval = setInterval(() => {
+      this.updateChartData(); 
+    }, 10000); 
+  }
 }
