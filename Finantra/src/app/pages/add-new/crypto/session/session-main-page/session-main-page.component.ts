71b2bc82ae273a418bb7d234dfff941ce48838d6
salvarez14 <saulalvarezfp@gmail.com>
2024-12-13 11:46:36 +0100

    Extracted presentation data

--- Finantra/src/app/pages/add-new/crypto/session/session-main-page/session-main-page.component.ts
+++ Finantra/src/app/pages/add-new/crypto/session/session-main-page/session-main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PresentationComponent } from "../../../../presentation/presentation.component";
 
 @Component({
   selector: 'app-session-main-page',
   standalone: true,
-  imports: [],
+  imports: [PresentationComponent],
   templateUrl: './session-main-page.component.html',
   styleUrl: './session-main-page.component.css'
 })
